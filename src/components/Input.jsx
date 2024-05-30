ee7a453c0442188bd6a478a23d72793cbbfaf934
NadavZ101 <nadavzivyon@gmail.com>
2024-05-30 16:25:28 +0300

    Added simple Input cmp & styling

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react"
+
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
+
+    return <p>
+        <label>{label}</label>
+        {textarea ?
+            <textarea ref={ref} {...props} />
+            :
+            <input ref={ref} {...props} />}
+    </p >
+})
+
+export { Input }
